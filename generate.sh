3de9361d16eda77c8ee00e9b4cd29ed7c69f0d6f
JustinCB <JustinCB@opmbx.org>
2018-07-16 14:17:42 -0400

    Update generate.sh

--- generate.sh
+++ generate.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 echo "#!/bin/sh" >functions.sh
 cat globus_perftest.ini collection_list.ini gateway_list.ini globus_ls.ini globus_stat.ini get_gcp.ini globus_wait.ini make_test_dataset.ini globus_transfer_helper.ini globus_transfer_test_helper.ini endpoint_test_helper.ini make_test_dataset.ini list_collection_names.ini list_names.ini list_types.ini list_uuid.ini list_roots.ini list_other_storage_gateways.ini >>functions.sh
-cat functions.sh endpoint_test.ini | awk 'NF != 0{sub(/^[\t ]*/,"");gsub(/#[A-TV-fhj-ln-rstvwz ][A-z0-9@\/\-\&,#\'\''\\\(\)"\$ ]+$/,"");gsub(/[\t ]*$/,"");print}' >endpoint_test.sh
+cat functions.sh endpoint_test.ini | awk 'NF != 0{sub(/^[\t ]*/,"");gsub(/#[A-TV-fhj-ln-rstvwz ][A-z0-9@\/\-\&,#\'\''\\\(\)"\$ ]+$/,"");gsub(/[\t ]*$/,"");if($0!=""){print}}' >endpoint_test.sh
 chmod +x endpoint_test.sh
-cat functions.sh test_globus.ini | awk 'NF != 0{sub(/^[\t ]*/,"");gsub(/#[A-TV-fhj-ln-rstvwz ][A-z0-9@\/\-\&,#\'\''\\\(\)"\$ ]+$/,"");gsub(/[\t ]*$/,"");print}' >test_globus.sh
+cat functions.sh test_globus.ini | awk 'NF != 0{sub(/^[\t ]*/,"");gsub(/#[A-TV-fhj-ln-rstvwz ][A-z0-9@\/\-\&,#\'\''\\\(\)"\$ ]+$/,"");gsub(/[\t ]*$/,"");if($0!=""){print}}' >test_globus.sh
 chmod +x test_globus.sh
 
